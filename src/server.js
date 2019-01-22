732de8d20a69e91857bbc74578b8562c673916a3
André Luiz Ximenes <andreluziximenes@gmail.com>
2019-01-21 23:29:04 -0300

    .

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path')
 
 
 // Import Routes
@@ -13,7 +14,7 @@ dotenv.config();
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/node_rest_api', { useNewUrlParser: true }); 
+mongoose.connect(process.env.MONGO_CONNECTION + process.env.DATABASE_NAME, { useNewUrlParser: true });
 
 
 // Server express config
@@ -21,15 +22,15 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/users', usersRoute);
+
 
 /* 
  Routes 
  (these routes are just examples)
 */
-app.get('/', (req, res) => { res.redirect('/server-info')})
+app.get('/', (req, res) => { res.redirect('/server-info') })
 app.get('/server-info', indexController.info);
-
+app.use('/users', usersRoute);
 
 // redirect uri errors to custom message
 app.use((req, res) => {
